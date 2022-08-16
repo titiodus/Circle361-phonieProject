00a3fbe11abd1ffa433723f0ca52f518653ac61e
Titilola Oduwole <titioduwole@gmail.com>
2022-08-16 18:16:15 +0000

    Updated the app.properties file with circle member details

--- app.mjs
+++ app.mjs
@@ -4,13 +4,15 @@ function startApp() {
     // so create and use/call additional functions from here
   
     // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
+console.log('make magic in here!');
   
     const header = document.querySelector('h2');
     if(header) {
       header.textContent = 'make some magic here!!';
     }
   };
+  
+    
   
   // ======= DO NOT EDIT ============== //
   export default startApp;
